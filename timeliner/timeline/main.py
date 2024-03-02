b6c0f67851e061bafd2c14883f4de01390c38e4d
Harry-QY <qy250@cam.ac.uk>
2024-03-02 19:14:20 +0000

    bananabananabanana

--- timeliner/timeline/main.py
+++ timeliner/timeline/main.py
@@ -13,11 +13,16 @@ chat_model = ChatMistralAI(
 
 template = """
 You are a timeline generator which genrates a timeline of a given object or activity and give an id for each event in timeline.
+Also include any notes and sources used in the result.
 
-Query: Give me a timeline of {query}, output as a JSON object such as
-    id:1, time: '2000', event: 'Event 1: Lorem ipsum dolor' ,
-    id:2, time: '2005', event: 'Event 2: Lorem ipsum dolor sit amet, conse',
-    id:3, time: '2010', event: 'Event 3: Lorem ipsum dolor sit amet, consectet',
+Query: Give me a timeline of {query}, output only a JSON object.
+Here is an example of the format of the JSON object:
+    {{id:1, time: '2000', event: 'Event 1: Lorem ipsum dolor'}},
+    {{id:2, time: '2005', event: 'Event 2: Lorem ipsum dolor sit amet, conse'}},
+    {{id:3, time: '2010', event: 'Event 3: Lorem ipsum dolor sit amet, consectet'}},
+    {{notes: 'any notes'}},
+    {{sources: 'any sources}}
+Do not include line changes in the output.
 """
 
 prompt_template = PromptTemplate(
@@ -30,7 +35,7 @@ def get_timeline(query):
     prompt = prompt_template.format(query = query)
     result = chat_model.invoke(prompt)
 
-    return jsonify(result.content)
+    return result.content
 
 
 
