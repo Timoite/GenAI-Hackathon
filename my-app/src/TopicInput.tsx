2cec391191d7e28f59ad1302f522f0acd8b48d96
timtan <116691346+iitealpha@users.noreply.github.com>
2024-03-02 15:37:39 +0000

    Interface done

--- my-app/src/TopicInput.tsx
+++ my-app/src/TopicInput.tsx
@@ -1,6 +1,6 @@
-// TopicInput.tsx
 import React, { useState, FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
+import logo from "./assets/timeliner.svg"; // Import the logo
 
 const TopicInput: React.FC = () => {
   const [topic, setTopic] = useState<string>('');
@@ -8,29 +8,29 @@ const TopicInput: React.FC = () => {
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (topic.trim()) { // Check if the topic is not just empty spaces
+    if (topic.trim()) {
       navigate('/timeline', { state: { topic } });
     }
   };
 
   return (
-    <div className="min-h-screen w-screen bg-gray-900 flex justify-center items-center p-0 m-0">
-      <form onSubmit={handleSubmit} className="w-full max-w-xs">
-        <div className="mb-4">
-          <label htmlFor="topic" className="block text-sm font-medium text-gray-300">Topic</label>
+    <div className="flex flex-col justify-center items-center h-screen w-screen bg-gray-900">
+      <img src={logo} alt="Logo" className="mb-4 w-32" /> {/* Adjust size as needed */}
+      <form onSubmit={handleSubmit} className="w-full max-w-lg mx-auto">
+        <div className="flex items-center relative">
           <input
-            type="text"
-            id="topic"
+            type="search"
+            id="default-search"
+            className="block w-full p-4 pl-10 text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-full focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+            placeholder="Search topics..."
+            required
             value={topic}
             onChange={(e) => setTopic(e.target.value)}
-            className="mt-1 block w-full border border-gray-600 bg-gray-800 text-white rounded-md py-2 px-3 shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            placeholder="Enter a topic"
-            required
           />
+          <button type="submit" className="absolute right-4 bottom-2.5 bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Submit
+          </button>
         </div>
-        <button type="submit" className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50">
-          Submit
-        </button>
       </form>
     </div>
   );
